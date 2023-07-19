d23705cf0de07200d98d35aa977b3b23dd9ee2d9
MansiChavda <106135937+MansiChavda@users.noreply.github.com>
2023-07-19 15:31:02 +0530

    Update base.py

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -159,6 +159,7 @@ STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesSto
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
